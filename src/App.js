d1a2f279622c37d303b9d7cd40620be7fedb7266
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-05-10 12:13:36 +0530

    adds about section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import  { ThemeProvider} from "styled-components"  //Themeprovider is a theme context 
+import styled, { ThemeProvider} from "styled-components"  //Themeprovider is a theme context 
 import {darkTheme} from "./utils/themes"
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import HeroSection from "./components/HeroSection/HeroSection"; 
+
+
+
+const Body = styled.div`
+background-color: ${({theme}) => theme.bg};
+width: 100%;
+height: 90vh;
+overflow-x: hidden;
+position: relative; 
+` 
+
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
         <Navbar/>
+        {/* <hr style={{'color': '#ffffff' }} /> */}
+        <Body>
+          <HeroSection/>
+        </Body>
      </BrowserRouter>
     </ThemeProvider>
   );
